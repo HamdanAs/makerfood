759a3a9b7fba26a8419e206700b4aadbc9990d58
HamdanAs <21hamdanas@gmail.com>
2022-11-04 15:19:46 +0700

    fix

--- resources/views/app/transaction/index.blade.php
+++ resources/views/app/transaction/index.blade.php
@@ -3,7 +3,7 @@
     <h1 class="header">Transaksi</h1>
   </x-slot>
 
-  <div class="mx-auto sm:px-6 lg:px-4 pt-7">
+  <div class="mx-auto px-10 pt-7">
     <h1 class="font-bold text-xl mb-3">Transaksi</h1>
     <div class="bg-white overflow-hidden sm:rounded-lg">
       <div class="p-6 bg-white">
